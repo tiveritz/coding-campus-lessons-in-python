a2ca774733510aad59b504647db98e4607be9ef7
tiveritz <lukas.walch@icloud.com>
2020-10-17 10:25:32 +0200

    Comment

--- oct/day09part02.py
+++ oct/day09part02.py
@@ -1,6 +1,7 @@
 from day09part01 import bubble_sort
 # import all: import day09part01 -> the call with: day09part01.bubble_sort()
 
+
 def recursion_counter(start, end):
         counter = start
         print(counter)
